b4e5c5bf7d3839c234d9780c8cfef2ab1e602b71
Diego Aguiar <diegoraguiar@gmail.com>
2017-09-20 00:52:59 -0300

    Inclusão do service de validação de inscrição estadual do estado do ALAGOAS

---
+++ src/app/validators/services/inscricao-estadual/alagoas.service.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { AlagoasService } from './alagoas.service';
+
+fdescribe('Service: AlagoasService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AlagoasService]
+    });
+  });
+
+  it('deve ter uma instancia criada', inject([AlagoasService], (service: AlagoasService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('deve estar valido', inject([AlagoasService], (service: AlagoasService) => {
+    expect(service.validar('240000048')).toBe(true);
+  }));
+
+  it('deve começar com 24 que é o código do estado', inject([AlagoasService], (service: AlagoasService) => {
+    expect(service.validar('240000048')).toBe(true);
+  }));
+
+  it('deve possuir o codigo da empresa igual a 0, ou 3, ou 5, ou 7, ou 8', inject([AlagoasService], (service: AlagoasService) => {
+    expect(service.validar('240000048')).toBe(true);
+    expect(service.validar('243103905')).toBe(true);
+    expect(service.validar('245211306')).toBe(true);
+    expect(service.validar('247961361')).toBe(true);
+    expect(service.validar('248575465')).toBe(true);
+  }));
+
+  it('deve possuir 9 digitos', inject([AlagoasService], (service: AlagoasService) => {
+    expect(service.validar('240000048')).toBe(true);
+    expect(service.validar('24877298')).toBe(false);
+    expect(service.validar('2487729885')).toBe(false);
+  }));
+});
---
+++ src/app/validators/services/inscricao-estadual/alagoas.service.ts
@@ -0,0 +1,60 @@
+export class AlagoasService {
+
+  private readonly TAMANHO_IE = 9;
+  private readonly TAMANHO_NUMERO = 8;
+  private readonly TAMANHO_DIGITOS = 1;
+
+  private readonly PESOS_DIGITO_UM = [9, 8, 7, 6, 5, 4, 3, 2];
+
+  /**
+   * Regra de validação aplicada: http://www.sintegra.gov.br/Cad_Estados/cad_AL.html
+   *
+   * @param inscricaoEstadual Número referente a inscrição estadual
+   */
+  validar(inscricaoEstadual: string) {
+
+    if (!this.isPrimeirosDigitosDoisQuatro(inscricaoEstadual)
+      || !this.isTipoEmpresaValido(inscricaoEstadual)
+      || this.isQuantidadeNumerosInvalida(inscricaoEstadual)) {
+      return false;
+    }
+
+    const numero = inscricaoEstadual.substring(inscricaoEstadual.length - this.TAMANHO_IE, this.TAMANHO_NUMERO);
+    const digito = inscricaoEstadual.substring(this.TAMANHO_NUMERO, inscricaoEstadual.length);
+
+    const digitoCalculado = this.calcularDigitoVerificador(numero, this.PESOS_DIGITO_UM);
+
+    return digito === digitoCalculado;
+  }
+
+  private isPrimeirosDigitosDoisQuatro(inscricaoEstadual: string) {
+    // https://jex.im/regulex/#!embed=false&flags=&re=%5E24
+    return /^24/.test(inscricaoEstadual);
+  }
+
+  private isQuantidadeNumerosInvalida(inscricaoEstadual: string) {
+    return inscricaoEstadual.length !== this.TAMANHO_IE;
+  }
+
+  private isTipoEmpresaValido(inscricaoEstadual: string) {
+    const tiposEmpresa = [0, 3, 5, 7, 8];
+    const posicaoTipoEmpresa = 2;
+    return tiposEmpresa.includes(Number(inscricaoEstadual.charAt(posicaoTipoEmpresa)));
+  }
+
+  private calcularDigitoVerificador(digitos: string, fatoresMultiplicadores: Array<number>) {
+    const modulo = 11;
+    const multiplicador = 10;
+    let soma = 0;
+
+    for (let indice = 0; indice < digitos.length; indice++) {
+      soma += Number(digitos.charAt(indice)) * fatoresMultiplicadores[indice];
+    }
+
+    const produto = soma * multiplicador;
+    const resto = produto % modulo;
+
+    return String(resto === 10 ? 0 : resto);
+  }
+
+}
