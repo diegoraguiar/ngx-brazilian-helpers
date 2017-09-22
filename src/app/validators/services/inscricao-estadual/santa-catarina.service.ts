cbde03300726b77767625f973cd729ba7e0d5e8f
Diego Aguiar <diegoraguiar@gmail.com>
2017-09-21 21:53:15 -0300

    Inclusão do service de validação do estado do SANTA CATARINA

---
+++ src/app/validators/services/inscricao-estadual/santa-catarina.service.ts
@@ -0,0 +1,47 @@
+export class SantaCatarinaService {
+
+  private readonly TAMANHO_IE = 9;
+  private readonly TAMANHO_NUMERO = 8;
+  private readonly TAMANHO_DIGITOS = 1;
+
+  private readonly PESOS_DIGITO_UM = [9, 8, 7, 6, 5, 4, 3, 2];
+
+  /**
+   * Regra de validação aplicada: http://www.sintegra.gov.br/Cad_Estados/cad_SC.html
+   *
+   * @param inscricaoEstadual Número referente a inscrição estadual
+   */
+  validar(inscricaoEstadual: string) {
+    // https://jex.im/regulex/#!embed=false&flags=&re=%5C.%7C%5Cs
+    const ieSemMascara = inscricaoEstadual.replace(/\.|\s/g, '');
+
+    if (this.isQuantidadeNumerosInvalida(ieSemMascara)) {
+      return false;
+    }
+
+    const numero = ieSemMascara.substring(ieSemMascara.length - this.TAMANHO_IE, this.TAMANHO_NUMERO);
+    const digito = ieSemMascara.substring(this.TAMANHO_NUMERO, ieSemMascara.length);
+
+    const digitoCalculado = this.calcularDigitoVerificador(numero, this.PESOS_DIGITO_UM);
+
+    return digito === digitoCalculado;
+  }
+
+  private isQuantidadeNumerosInvalida(inscricaoEstadual: string) {
+    return inscricaoEstadual.length !== this.TAMANHO_IE;
+  }
+
+  private calcularDigitoVerificador(digitos: string, fatoresMultiplicadores: Array<number>) {
+    const modulo = 11;
+    let soma = 0;
+
+    for (let indice = 0; indice < digitos.length; indice++) {
+      soma += Number(digitos.charAt(indice)) * fatoresMultiplicadores[indice];
+    }
+
+    const resto = soma % modulo;
+
+    return String(resto <= 1 ? 0 : modulo - resto);
+  }
+
+}
