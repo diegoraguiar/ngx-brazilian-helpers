581fde4e5e89c217d84d3a9e96d0d2b3765a1780
Diego Aguiar <diegoraguiar@gmail.com>
2017-09-21 23:46:13 -0300

    Inclusão do service de validação do estado do MATO GROSSO DO SUL

---
+++ src/app/validators/services/inscricao-estadual/mato-grosso-sul.service.ts
@@ -0,0 +1,60 @@
+export class MatoGrossoSulService {
+
+  private readonly TAMANHO_IE = 9;
+  private readonly TAMANHO_NUMERO = 8;
+  private readonly TAMANHO_DIGITOS = 1;
+
+  private readonly PESOS_DIGITO_UM = [9, 8, 7, 6, 5, 4, 3, 2];
+
+  /**
+   * Regra de validação aplicada: http://www.sintegra.gov.br/Cad_Estados/cad_PA.html
+   *
+   * @param inscricaoEstadual Número referente a inscrição estadual
+   */
+  validar(inscricaoEstadual: string) {
+    // https://jex.im/regulex/#!embed=false&flags=&re=%5C-%7C%5Cs
+    const ieSemMascara = inscricaoEstadual.replace(/\-|\s/g, '');
+
+    if (!this.isPrimeirosDigitosDoisOito(ieSemMascara)
+      || this.isQuantidadeNumerosInvalida(ieSemMascara)) {
+      return false;
+    }
+
+    const numero = ieSemMascara.substring(ieSemMascara.length - this.TAMANHO_IE, this.TAMANHO_NUMERO);
+    const digito = ieSemMascara.substring(this.TAMANHO_NUMERO, ieSemMascara.length);
+
+    const digitoCalculado = this.calcularDigitoVerificador(numero, this.PESOS_DIGITO_UM);
+
+    return digito === digitoCalculado;
+  }
+
+  private isPrimeirosDigitosDoisOito(inscricaoEstadual: string) {
+    // https://jex.im/regulex/#!embed=false&flags=&re=%5E28
+    return /^28/.test(inscricaoEstadual);
+  }
+
+  private isQuantidadeNumerosInvalida(inscricaoEstadual: string) {
+    return inscricaoEstadual.length !== this.TAMANHO_IE;
+  }
+
+  private calcularDigitoVerificador(digitos: string, fatoresMultiplicadores: Array<number>) {
+    const modulo = 11;
+    let soma = 0;
+
+    for (let indice = 0; indice < digitos.length; indice++) {
+      soma += Number(digitos.charAt(indice)) * fatoresMultiplicadores[indice];
+    }
+
+    const resto = soma % modulo;
+    let digito = 0;
+    if (resto > 0) {
+      const resultado = modulo - resto;
+      if (resultado < 10) {
+        digito = resultado;
+      }
+    }
+
+    return String(digito);
+  }
+
+}
